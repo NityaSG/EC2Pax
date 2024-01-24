2433355e4799266834031786ec70b69d3798048f
Nitya Singh <76156577+NityaSG@users.noreply.github.com>
2024-01-24 08:48:05 +0530

    push

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import dotenv
 import pandas as pd
-from pandasai import Agent 
+from pandasai import Agent , SmartDataframe
 from flask import Flask, request, jsonify
 from pandasai.llm import OpenAI
 dotenv.load_dotenv()
@@ -9,8 +9,8 @@ key = os.getenv('OPENAI_API_KEY')
 
 df = pd.read_csv('processed_combined_new (1996 included)v.csv')
 llm1 = OpenAI(api_token=key)
-
-agent=Agent([df],config={"llm": llm1,"verbose": True,"enable_cache":False,"custom_instructions":"Please filter the data by column : year of record. If the answer is not available in the dataframe then decline to answer."})
+#sdf=SmartDataframe(df)
+agent=SmartDataframe(df,description="A census dataframe which contains information about south africa where it is essential to filter data as per the user requirements",config={"llm": llm1,"verbose": True,"enable_cache":False,"custom_instructions":"First thing to do is to filter the data by column : year of record. If not mentioned then please filter the data by the year 2022 If the answer is not available in the dataframe then decline to answer."})
 
 #agent.chat("what is the population of swartland")
 
